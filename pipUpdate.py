8e3b59bfe3d3fcea577f86e09e354aa62e5040a0
ha <84624971+HuzunluArtemis@users.noreply.github.com>
2022-01-23 17:53:17 +0300

    3

--- pipUpdate.py
+++
@@ -1,3 +0,0 @@
-import subprocess, pkg_resources
-packages = [dist.project_name for dist in pkg_resources.working_set]
-subprocess.call("pip install --upgrade " + ' '.join(packages), shell=True)
--- update.py
+++ update.py
@@ -4,7 +4,7 @@ from os import path as ospath, environ
 from subprocess import run as srun
 from requests import get as rget
 from dotenv import load_dotenv
-
+import subprocess, pkg_resources
 if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
@@ -12,6 +12,9 @@ if ospath.exists('log.txt'):
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=logging.INFO)
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+subprocess.call("pip install --upgrade " + ' '.join(packages), shell=True)
 
 CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL', None)
 try:
