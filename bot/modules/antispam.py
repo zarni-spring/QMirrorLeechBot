a9eb078714df324053bba8cd9be0bd4b2c79821e
= <84624971+HuzunluArtemis@users.noreply.github.com>
2022-01-29 00:18:47 +0300

    x

--- bot/modules/antispam.py
+++ bot/modules/antispam.py
@@ -1,11 +1,66 @@
 # written by huzunluartemis
 
+from cmath import inf
 from threading import Thread
 from bot import COMBOT_CAS_ANTISPAM, INTELLIVOID_ANTISPAM, LOGGER, SPAMWATCH_ANTISPAM_API, USERGE_ANTISPAM_API, dispatcher, app
 from bot.helper.telegram_helper.message_utils import auto_delete_message, sendMessage
 from telegram.ext import ChatMemberHandler, CallbackContext
 from telegram import Update
 import requests
+from telegram.message import Message
+from telegram.update import Update
+from telegram.error import RetryAfter
+from pyrogram.errors import FloodWait
+from time import sleep
+from asyncore import dispatcher
+import logging
+from turtle import up
+from typing import Tuple, Optional
+
+from telegram import Update, Chat, ChatMember, ParseMode, ChatMemberUpdated
+import telegram
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackContext,
+    ChatMemberHandler,
+)
+
+
+def extract_status_change(
+    chat_member_update: ChatMemberUpdated,
+) -> Optional[Tuple[bool, bool]]:
+    """Takes a ChatMemberUpdated instance and extracts whether the 'old_chat_member' was a member
+    of the chat and whether the 'new_chat_member' is a member of the chat. Returns None, if
+    the status didn't change.
+    """
+    status_change = chat_member_update.difference().get("status")
+    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
+
+    if status_change is None:
+        return None
+
+    old_status, new_status = status_change
+    was_member = (
+        old_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (old_status == ChatMember.RESTRICTED and old_is_member is True)
+    )
+    is_member = (
+        new_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (new_status == ChatMember.RESTRICTED and new_is_member is True)
+    )
+
+    return was_member, is_member
 
 
 def SpamWatchAntiSpamCheck(userid):
@@ -28,8 +83,11 @@ def SpamWatchAntiSpamCheck(userid):
         if reason: info += f"\nReason: {reason}"
         if message: info += f"\nMessage: {message}"
         if date: info += f"\nDate: {date}"
-    except: pass
-    return info
+        LOGGER.info(info)
+        return info
+    except Exception as e:
+        LOGGER.error(e)
+        return None
 
 
 def CombotAntiSpamCheck(userid):
@@ -48,9 +106,13 @@ def CombotAntiSpamCheck(userid):
         time_added = result['time_added']
         info += f"\nOffenses: {offenses}"
         info += f"\nTime: {time_added}"
-        info += f"\nCheck Link: https://cas.chat/query?u={userid}"
-    except: pass
-    return info
+        info += f"\nLink: <a href='https://cas.chat/query?u={userid}'>CAS Report</a>"
+        info += f"\nCopy: <code>https://cas.chat/query?u={userid}</code>"
+        LOGGER.info(info)
+        return info
+    except Exception as e:
+        LOGGER.error(e)
+        return None
 
 
 def UsergeAntiSpamCheck(userid):
@@ -70,8 +132,11 @@ def UsergeAntiSpamCheck(userid):
         if date: info += f"\nDate: {date}"
         if bb_user_name: info += f"\nBanned by: {bb_user_name}"
         if bb_user_id: info += f" <a href='tg://user?id={bb_user_id}'>({str(bb_user_id)})</a>"
-    except: pass
-    return info
+        LOGGER.info(info)
+        return info
+    except Exception as e:
+        LOGGER.error(e)
+        return None
 
 
 def IntelliVoidSpamCheck(userid):
@@ -99,8 +164,24 @@ def IntelliVoidSpamCheck(userid):
         if ham_prediction: info += f"\nHam Prediction: {ham_prediction}"
         if last_updated: info += f"\nLast Updated: {last_updated}"
         if language: info += f"\nLanguage: {language}"
-    except: pass
-    return info
+        LOGGER.info(info)
+        if bool(is_potential_spammer) or bool(is_blacklisted): return info
+        else: return None
+    except Exception as e:
+        LOGGER.error(e)
+        return None
+
+
+def send_message(text: str, context, groupid):
+    try:
+        return context.bot.send_message(groupid.id, text, parse_mode='html', disable_web_page_preview=True)
+    except RetryAfter as r:
+        LOGGER.warning(str(r))
+        sleep(r.retry_after * 1.5)
+        return send_message(text, context, groupid)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return
 
 
 def antispam(update: Update, context: CallbackContext) -> None:
@@ -108,12 +189,14 @@ def antispam(update: Update, context: CallbackContext) -> None:
     group = update.effective_chat
     a = context.bot.get_chat_member(group.id, context.bot.id).can_restrict_members
     if not a: return LOGGER.warning("Give ban permission to bot for spam api.")
-    # result = extract_status_change(update.chat_member)
-    # if result is None: return
-    # was_member, is_member = result
+    chat = update.effective_chat
+    if not chat.type in [Chat.GROUP, Chat.SUPERGROUP]: return # only work for groups
+    result = extract_status_change(update.chat_member)
+    if result is None: return
+    was_member, is_member = result
+    if was_member and not is_member: return # user leaved
     cause_name = update.chat_member.from_user.mention_html()
     member_name = update.chat_member.new_chat_member.user.mention_html()
-    # if was_member and (not is_member): return
     banned = None
     if SPAMWATCH_ANTISPAM_API: banned = SpamWatchAntiSpamCheck(update.chat_member.new_chat_member.user.id)
     if not banned:
@@ -122,9 +205,8 @@ def antispam(update: Update, context: CallbackContext) -> None:
         if USERGE_ANTISPAM_API: banned = UsergeAntiSpamCheck(update.chat_member.new_chat_member.user.id)
     if not banned:
         if INTELLIVOID_ANTISPAM: banned = IntelliVoidSpamCheck(update.chat_member.new_chat_member.user.id)
-    if not banned: LOGGER.info(f"User is clean: {str(update.chat_member.new_chat_member.user.full_name)}")
+    if not banned: return LOGGER.info(f"User is clean: {str(update.chat_member.new_chat_member.user.full_name)}")
     else:
-        LOGGER.info(banned)
         try:
             app.ban_chat_member(group.id, update.chat_member.new_chat_member.user.id)
             success = "Success"
@@ -133,12 +215,10 @@ def antispam(update: Update, context: CallbackContext) -> None:
             LOGGER.error(o)
         success = "Unsuccess"
         swtc = f"{cause_name} Added: {member_name}"
-        swtc += f"\nID: <code>{str(update.chat_member.new_chat_member.user.id)}<\code>"
+        swtc += f"\nID: <code>{str(update.chat_member.new_chat_member.user.id)}</code>"
         swtc += f"\nBan: {success}"
-        swtc += f"\n\n{banned}"
-        try: sendMessage(swtc, context.bot, update)
-        except Exception as t: LOGGER.error(t)
-        # Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
+        swtc += f"\n{banned}"
+        send_message(swtc, context, group)
 
 if SPAMWATCH_ANTISPAM_API or COMBOT_CAS_ANTISPAM or USERGE_ANTISPAM_API or INTELLIVOID_ANTISPAM:
     antispam_handler = ChatMemberHandler(antispam, ChatMemberHandler.CHAT_MEMBER, run_async=True)
